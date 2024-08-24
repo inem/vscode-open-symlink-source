97c8c164f3f02fef05aff6c908254a159e60dcec
Ivan Nemytchenko <nemytchenko@gmail.com>
2024-08-24 11:02:48 +0200

    show-symlink-source

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,72 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as path from 'path';
+
+let statusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "openSymlinkSource" is now active!');
+    console.log('Extension "showSymlinkSource" is now active');
 
-	let disposable = vscode.commands.registerCommand('open-symlink-source.openSymlinkSource', () => {
-		var currentlyOpenTabfilePath = vscode.window.activeTextEditor?.document.fileName || '';
-		fs.realpath(currentlyOpenTabfilePath, undefined, (err, resolvedPath) => {
-			if (err) { throw err; };
-			vscode.window.showInformationMessage(resolvedPath);
-			let uri = vscode.Uri.file(resolvedPath);
-			vscode.commands.executeCommand('vscode.open', uri);
-		});
-	});
+    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+    context.subscriptions.push(statusBarItem);
 
-	context.subscriptions.push(disposable);
+    function updateStatusBar() {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            statusBarItem.hide();
+            return;
+        }
+
+        const filePath = editor.document.fileName;
+        const workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
+
+        fs.lstat(filePath, (err, stats) => {
+            if (err) {
+                console.error(`Error checking file: ${err.message}`);
+                statusBarItem.hide();
+                return;
+            }
+
+            if (stats.isSymbolicLink()) {
+                fs.realpath(filePath, (err, resolvedPath) => {
+                    if (err) {
+                        statusBarItem.text = `$(error) ${err.message}`;
+                    } else {
+                        let displayPath = resolvedPath;
+                        if (workspaceFolder) {
+                            displayPath = path.relative(workspaceFolder.uri.fsPath, resolvedPath);
+                            // If the path is not within the workspace, it will start with '..'
+                            if (!displayPath.startsWith('..')) {
+                                displayPath = './' + displayPath;
+                            }
+                        }
+                        statusBarItem.text = `$(link) ${displayPath}`;
+                    }
+                    statusBarItem.tooltip = 'Path to the symlink source file';
+                    statusBarItem.show();
+                });
+            } else {
+                statusBarItem.hide();
+            }
+        });
+    }
+
+    // Update status bar when extension is activated
+    updateStatusBar();
+
+    // Update status bar when active editor changes
+    context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBar));
+
+    // Update status bar when document is saved
+    context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(() => {
+        if (vscode.window.activeTextEditor) {
+            updateStatusBar();
+        }
+    }));
 }
 
-export function deactivate() {}
+export function deactivate() {
+    if (statusBarItem) {
+        statusBarItem.dispose();
+    }
+}
